1d8d7c453f4d2c0d85b38325c156ea842b43d8a3
DOM <szpadlicho@gmail.com>
2014-11-16 21:30:13 +0100

    .

--- index.php
+++ index.php
@@ -18,94 +18,10 @@
         <link title="deafult" type="text/css" rel="stylesheet" href="css/content.css" />
         
         <script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
-        <script type="text/javascript" src="js/scrypt.js"></script>
         <script type="text/javascript" src="js/stars.js" ></script>
+        <script type="text/javascript" src="js/scrypt.js"></script>
 
         <script type="text/javascript">
-            /*długość okna przegladarki*/
-            var both = function () {
-                // debugger
-                var htt = $(window).height();
-                var wtt = $(window).width();
-                $('#count').css({'color':'white'});
-                $('#count').text(wtt+'/'+htt);
-                // setup
-                $( '#relative-holder' ).css({'width':wtt+'px','height':htt+'px'});
-            };
-            $(document).ready(both);
-            $(document).load(both);
-            $(window).resize(both);
-            $(document).ready(function(){
-                var center_ratio = 40;
-                var satellite_ratio = 10;
-                var size = parseInt($( 'body' ).css('font-size'));
-                /*
-                $( '#home' ).click(function(){
-                    $( '#earth, #jupiter, #saturn' ).removeClass( 'zoom' ).addClass( 'satellite' );
-                    $( '.satellite' ).removeClass( 'paused' ).addClass( 'running' );
-                    $( '#earth, #jupiter, #saturn, .center, .satellite' ).removeAttr( 'style' );
-
-                    $( '.content' ).removeClass( 'show right-p left-p' ).addClass( 'hidden' );
-                });
-                */
-                $( '#home' ).click(function(){
-                    $( '#earth, #jupiter, #saturn' ).removeClass( 'zoom' ).addClass( 'satellite' );
-                    $( '.center, .satellite' ).css({'left':'', 'top':'','width':'','height':'','transition':'300ms linear'});/*i add only this*/
-                    $( '.satellite' ).removeClass( 'paused' ).addClass( 'running' );
-                    $( '#earth, #jupiter, #saturn' ).removeAttr( 'style' );
-                    setTimeout(function() {
-                        $( '.center, .satellite' ).removeAttr( 'style' );    
-                    }, 300);
-
-                    $( '.content' ).removeClass( 'show right-p left-p' ).addClass( 'hidden' );
-                });
-                $( '#about' ).click(function(){
-                    if ( $( '#sun' ).hasClass('paused') ) {
-                        //$( '#home' ).click();
-                        $( '#earth, #jupiter, #saturn' ).removeClass( 'zoom' ).addClass( 'satellite' ).removeAttr( 'style' );
-                        $( '.content' ).removeClass( 'show right-p left-p' ).addClass( 'hidden' );
-                    }
-                    $( '#earth' ).removeClass( 'satellite' ).addClass( 'zoom' );
-                    $( '.center, .satellite' ).removeClass( 'running' ).addClass( 'paused' ).css({'width':'0','height':'0'});
-                    $( '#earth' ).css({'left':'-20em', 'top':'-20em', 'width':'40em','height':'40em','transition':'300ms linear','animation-name':'none'});
-                    
-                    $( '#about-left' ).removeClass( 'hidden' ).addClass( 'show left-p' );
-                    $( '#about-right' ).removeClass( 'hidden' ).addClass( 'show right-p' );
-                    
-                });
-                $( '#links' ).click(function(){
-                    if ( $( '#sun' ).hasClass('paused') ) {
-                        //$( '#home' ).click();
-                        $( '#earth, #jupiter, #saturn' ).removeClass( 'zoom' ).addClass( 'satellite' ).removeAttr( 'style' );
-                        $( '.content' ).removeClass( 'show right-p left-p' ).addClass( 'hidden' );
-                    }
-                    $( '#jupiter' ).removeClass( 'satellite' ).addClass( 'zoom' );
-                    $( '.center, .satellite' ).removeClass( 'running' ).addClass( 'paused' ).css({'width':'0','height':'0'});
-                    $( '#jupiter' ).css({'left':'-20em', 'top':'-20em', 'width':'40em','height':'40em','transition':'300ms linear','animation-name':'none'});
-                    
-                    $( '#links-left' ).removeClass( 'hidden' ).addClass( 'show left-p' );
-                    $( '#links-right' ).removeClass( 'hidden' ).addClass( 'show right-p' );
-                });
-                $( '#contact' ).click(function(){
-                    if ( $( '#sun' ).hasClass('paused') ) {
-                        //$( '#home' ).click();
-                        $( '#earth, #jupiter, #saturn' ).removeClass( 'zoom' ).addClass( 'satellite' ).removeAttr( 'style' );
-                        $( '.content' ).removeClass( 'show right-p left-p' ).addClass( 'hidden' );
-                    }
-                    $( '#saturn' ).removeClass( 'satellite' ).addClass( 'zoom' );
-                    $( '.center, .satellite' ).removeClass( 'running' ).addClass( 'paused' ).css({'width':'0','height':'0'});
-                    $( '#saturn' ).css({'left':'-20em', 'top':'-10em', 'width':'40em','height':'20em','transition':'300ms linear','animation-name':'none'});
-                    
-                    $( '#contact-left' ).removeClass( 'hidden' ).addClass( 'show left-p' );
-                    $( '#contact-right' ).removeClass( 'hidden' ).addClass( 'show right-p' );
-                });
-                /**/
-                $( '.project-link' ).hover(function(){
-                    var url = $(this).attr( 'href' );
-                    $( '#links-right' ).html( '<img src="images/loading.gif"> loading...' );
-                    $( '#links-right' ).html( '<object width="100%" height="100%" type="text/html" data="'+url+'"/>' );
-                });
-            });
 
         </script>
     </head>
@@ -114,10 +30,10 @@
         <section id="relative-holder">
             <header>
                 <nav id='menu' onmousedown="context.fillStyle='rgba(0,0,0,'+opacity+')'; star_speed=15.5;" onmouseup="frezz(200)">
-                    <a id="home" class="button">Home</a>
-                    <a id="about" class="button">About Me</a>
-                    <a id="links" class="button">Projects</a>
-                    <a id="contact" class="button">Contact</a>
+                    <a id="home" class="button">Start</a>
+                    <a id="about" class="button">O mnie</a>
+                    <a id="links" class="button">Projekty</a>
+                    <a id="contact" class="button">Kontakt</a>
                 </nav>
             </header>
             <article>
@@ -168,7 +84,7 @@
                         <p class="ak">Program do wstawiania znaku wodnego w grafikę.</p>
                         <p class="cn"><a class="project-link" title="" target="_blank" href="http://www.znak.szpadlic.bdl.pl">www.znak.szpadlic.bdl.pl</a></p>
                     </div>
-                    <div id="links-right" class="content right hidden">Preview</div>
+                    <div id="links-right" class="content right hidden">Podgląd</div>
                     <div id="contact-left" class="content left hidden" >szpadlicho@gmail.com</div>
                     <div id="contact-right" class="content right hidden" >kom. 88895877</div>
                 </div>
--- js/scrypt.js
+++ js/scrypt.js
@@ -1,50 +1,76 @@
-//-----funkcja do opuzniania linkow-----
-function link(where, ile){
-	setTimeout(function(){
-		window.location.href = where;
-	},ile)
-}
-//----animacja powiekszjaca obrazek-----
-function anime(what, size, pos){
-	document.getElementById(what).style.width=size;
-	$('#'+pos).css({"top":"90px", "left":"220px", "z-index":"2"});
-	//$('#'+pos+'_div').css({"display":"none"});	$('body').css({"background-image":"url('images/stars.jpg')","background-repeat":"no-repeat","background-attachment":"fixed","background-position":"center","-webkit-background-size":"cover","-moz-background-size":"cover","-o-background-size":"cover","background-size":"cover","animation":"gwiazdy 9000s linear infinite alternate"});
-	//$("body").css({"animation":"gwiazdy 20s linear infinite alternate","font-size":"1em"});
-}
-//----animacja zmniejszajaca obrazki----
-function offset(what1,what2,what3,what4,what5,what6,what7,what8, size){
-	document.getElementById(what1).style.width=size;
-	document.getElementById(what2).style.width=size;
-	document.getElementById(what3).style.width=size;
-	document.getElementById(what4).style.width=size;
-	document.getElementById(what5).style.width=size;
-	document.getElementById(what6).style.width=size;
-	document.getElementById(what7).style.width=size;
-	document.getElementById(what8).style.width=size;	
-}
-//---animacja przeladowania strony----
-$(document).ready(function() {
-        var overload_time = 700;/*czas animacji*/
-        $('body').hide();
-        $('body').fadeIn(overload_time);
-        $('#button').click(/*orginalnie było $('a').click( */
-        function()
-        {
-                var href = $(this).attr("href");
-                
-                var xx = document.referrer;
-                if (href.indexOf("http") > -1) {
-                        var url = href.match(/:\/\/(www\.)?(.[^/:]+)/)[2];
-                        if (url == window.location.hostname) {
-                                $('body').fadeOut(overload_time);
-                        }
-                } else {
-                        $('body').fadeOut(overload_time);
-                }
-                
-                if (href.indexOf("./") > -1) {
-                        $('body').fadeIn(overload_time);
-                }
-                return true;
-        })
+/*
+ * width browser window 
+*/
+var both = function () {
+    // debugger
+    var htt = $(window).height();
+    var wtt = $(window).width();
+    $('#count').css({'color':'white'});
+    $('#count').text(wtt+'/'+htt);
+    // setup
+    $( '#relative-holder' ).css({'width':wtt+'px','height':htt+'px'});
+};
+$(document).ready(both);
+$(document).load(both);
+$(window).resize(both);
+$(document).ready(function(){
+    /*
+     * click events
+    */
+    $( '#home' ).click(function(){
+        $( '#earth, #jupiter, #saturn' ).removeClass( 'zoom' ).addClass( 'satellite' );
+        $( '.center, .satellite' ).css({'left':'', 'top':'','width':'','height':'','transition':'300ms linear'});/*i add only this*/
+        $( '.satellite' ).removeClass( 'paused' ).addClass( 'running' );
+        $( '#earth, #jupiter, #saturn' ).removeAttr( 'style' );
+        setTimeout(function() {
+            $( '.center, .satellite' ).removeAttr( 'style' );    
+        }, 300);
+
+        $( '.content' ).removeClass( 'show right-p left-p' ).addClass( 'hidden' );
+    });
+    $( '#about' ).click(function(){
+        if ( $( '#sun' ).hasClass('paused') ) {
+            $( '#earth, #jupiter, #saturn' ).removeClass( 'zoom' ).addClass( 'satellite' ).removeAttr( 'style' );
+            $( '.content' ).removeClass( 'show right-p left-p' ).addClass( 'hidden' );
+        }
+        $( '#earth' ).removeClass( 'satellite' ).addClass( 'zoom' );
+        $( '.center, .satellite' ).removeClass( 'running' ).addClass( 'paused' ).css({'width':'0','height':'0'});
+        $( '#earth' ).css({'left':'-20em', 'top':'-20em', 'width':'40em','height':'40em','transition':'300ms linear','animation-name':'none'});
+        
+        $( '#about-left' ).removeClass( 'hidden' ).addClass( 'show left-p' );
+        $( '#about-right' ).removeClass( 'hidden' ).addClass( 'show right-p' );
+        
+    });
+    $( '#links' ).click(function(){
+        if ( $( '#sun' ).hasClass('paused') ) {
+            $( '#earth, #jupiter, #saturn' ).removeClass( 'zoom' ).addClass( 'satellite' ).removeAttr( 'style' );
+            $( '.content' ).removeClass( 'show right-p left-p' ).addClass( 'hidden' );
+        }
+        $( '#jupiter' ).removeClass( 'satellite' ).addClass( 'zoom' );
+        $( '.center, .satellite' ).removeClass( 'running' ).addClass( 'paused' ).css({'width':'0','height':'0'});
+        $( '#jupiter' ).css({'left':'-20em', 'top':'-20em', 'width':'40em','height':'40em','transition':'300ms linear','animation-name':'none'});
+        
+        $( '#links-left' ).removeClass( 'hidden' ).addClass( 'show left-p' );
+        $( '#links-right' ).removeClass( 'hidden' ).addClass( 'show right-p' );
+    });
+    $( '#contact' ).click(function(){
+        if ( $( '#sun' ).hasClass('paused') ) {
+            $( '#earth, #jupiter, #saturn' ).removeClass( 'zoom' ).addClass( 'satellite' ).removeAttr( 'style' );
+            $( '.content' ).removeClass( 'show right-p left-p' ).addClass( 'hidden' );
+        }
+        $( '#saturn' ).removeClass( 'satellite' ).addClass( 'zoom' );
+        $( '.center, .satellite' ).removeClass( 'running' ).addClass( 'paused' ).css({'width':'0','height':'0'});
+        $( '#saturn' ).css({'left':'-20em', 'top':'-10em', 'width':'40em','height':'20em','transition':'300ms linear','animation-name':'none'});
+        
+        $( '#contact-left' ).removeClass( 'hidden' ).addClass( 'show left-p' );
+        $( '#contact-right' ).removeClass( 'hidden' ).addClass( 'show right-p' );
+    });
+    /*
+     * load site dynamically in div
+    */
+    $( '.project-link' ).hover(function(){
+        var url = $(this).attr( 'href' );
+        $( '#links-right' ).html( '<img src="images/loading.gif"> loading...' );
+        $( '#links-right' ).html( '<object width="100%" height="100%" type="text/html" data="'+url+'"/>' );
+    });
 });
